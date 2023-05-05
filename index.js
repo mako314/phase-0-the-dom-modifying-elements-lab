81feb72bcb9c4ef7b88d2ab810c06f198e6c19ab
mako314 <119079347+mako314@users.noreply.github.com>
2023-05-04 21:44:14 -0400

    remove main, add header variable to point correctly

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainNode = document.querySelector("main")
+//console.log(mainNode)
+mainNode.remove();
+
+const newHeader = document.querySelector("h1")
+console.log(newHeader)
+newHeader.id = 'victory'
+newHeader.textContent = "Mako is the champion"
